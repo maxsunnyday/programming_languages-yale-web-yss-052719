a690b3d50593026fe9d1af16a56c2d743765bd64
Max Sun <max.sun@yale.edu>
2019-05-18 03:34:58 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,25 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  style_array =[]
+  languages.each do |style, hash|
+    hash.each do |language, another_hash|
+      if style_array != []
+        style_array << style
+        new_hash[language] = {
+          :type => another_hash[:type],
+          :style => style_array
+        }
+      else
+        style_array = []
+        style_array << style
+        new_hash[language] = {
+          :type => another_hash[:type],
+          :style => style_array
+        }
+      end
+
+    end
+  end
+  new_hash
 end
