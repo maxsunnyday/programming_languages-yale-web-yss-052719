13627ce7392eaae9857a692f937e677b79a3d178
Max Sun <max.sun@yale.edu>
2019-05-18 03:49:46 +0000

    Programming_languages

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, hash|
+    hash.each do |language, another_hash|
+      keys = new_hash.keys
+      if keys.include?(language)
+        new_hash[language][:style] << style
+      else
+        style_array = []
+        style_array << style
+        new_hash[language] = {
+          :type => another_hash[:type],
+          :style => style_array
+        }
+      end
+    end
+  end
+  new_hash
 end
