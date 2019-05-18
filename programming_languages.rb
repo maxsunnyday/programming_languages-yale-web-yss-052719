a1d5276a4dddcd769fca7b2743ea491d7b89db75
Max Sun <max.sun@yale.edu>
2019-05-18 03:39:47 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, hash|
     hash.each do |language, another_hash|
+      binding.pry
       style_array = []
       style_array << style
       new_hash[language] = {
